91688d05ea7f3d71ceb905b94984787925a345b2
sunnysmilen <davidsalnicole@gmail.com>
2025-06-04 16:22:07 +0200

    creation of routind advanced

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,41 @@
+// Import access to data
+import categoryRepository from "./categoryRepository";
+
+// Some data to make the trick
+
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+// Declare the actions
+
+import type { RequestHandler } from "express";
+
+const browser: RequestHandler = async (req, res) => {
+  const categoriesFromDB = await categoryRepository.readAll();
+
+  res.json(categoriesFromDB);
+};
+
+const reads: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+
+  const category = categories.find((c) => c.id === parsedId);
+
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+// Export them to import them somewhere else
+
+export default { browser, reads };
